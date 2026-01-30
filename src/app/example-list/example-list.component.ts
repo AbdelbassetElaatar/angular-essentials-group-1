3dcfb7979b94f7c2be3f6f51f5f54165cabff556
Mustapha Bouddahr <mustaphabouddahr.dev@gmail.com>
2026-01-30 17:28:42 +0100

    ajouter le styling de le components tab-button & example-list

--- src/app/example-list/example-list.component.ts
+++ src/app/example-list/example-list.component.ts
@@ -6,36 +6,38 @@ import { TabButtonComponent } from '../tab-button/tab-button.component';
   standalone: true,
   imports: [TabButtonComponent],
   template: `
-    <h1>Examples</h1>
-    <div class="button-group">
-      @for (content of contents(); track content.title) {
-        <app-tab-button 
-          [label]="content.title"
-          [isSelected]="selectedTopic === content.title"
-          (buttonClick)="onSelectTopic(content.title)" />
-      }
-    </div>
-    <div class="example-content">
-      @if (selectedTopic) {
-        <h2>{{ getSelectedContent()?.title }}</h2>
-        <p>{{ getSelectedContent()?.description }}</p>
-        <pre>{{ getSelectedContent()?.code }}</pre>
-      } @else {
-        <p>Please select a topic</p>
-      }
+    <div>
+      <h1>Examples</h1>
+      <div class="button-group">
+        @for (content of contents(); track content.title) {
+          <app-tab-button
+            [label]="content.title"
+            [isSelected]="selectedTopic === content.title"
+            (buttonClick)="onSelectTopic(content.title)" />
+        }
+      </div>
+      <div class="example-content">
+        @if (selectedTopic) {
+          <h2>{{ getSelectedContent()?.title }}</h2>
+          <p>{{ getSelectedContent()?.description }}</p>
+          <pre>{{ getSelectedContent()?.code }}</pre>
+        } @else {
+          <p>Please select a topic</p>
+        }
+      </div>
     </div>
   `,
   styleUrl: './example-list.component.css'
 })
 export class ExampleListComponent {
   contents = input.required<{ title: string; description: string, code?: string }[]>();
-  
+
   selectedTopic: string | null = null;
-  
+
   onSelectTopic(topic: string) {
     this.selectedTopic = topic;
   }
-  
+
   getSelectedContent() {
     return this.contents().find(content => content.title === this.selectedTopic) || null;
   }
--- src/app/tab-button/tab-button.component.ts
+++ src/app/tab-button/tab-button.component.ts
@@ -3,24 +3,15 @@ import { Component, input, output } from '@angular/core';
 @Component({
   selector: 'app-tab-button',
   standalone: true,
-  template: `
-    <button 
-      (click)="handleClick()"
-      [class.active]="isSelected()">
-      {{ label() }}
-    </button>
-  `,
+  templateUrl: './tab-button.component.html',
   styleUrl: './tab-button.component.css'
 })
 export class TabButtonComponent {
-  // What inputs do you need?
   label = input.required<string>();
   isSelected = input<boolean>(false);
-  
-  // What output do you need?
+
   buttonClick = output<void>();
-  
-  // What method handles the click?
+
   handleClick() {
     this.buttonClick.emit();
   }
